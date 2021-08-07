5e03028c36a2996c6778d69fa93fdd6be65be30f
Sean Sullivan <sullivansean27@gmail.com>
2021-08-07 02:35:25 -0400

    move some stuff in web around

--- web/server/run.sh
+++
@@ -1,2 +0,0 @@
-npx ssvmup@0.1.20 build --target deno ./wasm-chess && \
-deno run --unstable --allow-read --allow-net --allow-env --importmap src/import_map.json src/server.ts
---
+++ web/wasm-chess/build.sh
@@ -0,0 +1,1 @@
+npx rustwasmc build --target deno ./
