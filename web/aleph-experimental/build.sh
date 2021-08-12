8896ad43f5588fd772b1248fea070dda5380eca2
snen <seansullygames@gmail.com>
2021-08-12 13:01:04 -0400

    move aleph experimental into its own subdirectory

--- web/aleph-experimental/build.sh
+++ web/aleph-experimental/build.sh
@@ -1,5 +1,3 @@
-cd ~/grovest/chess
-
 if [ -z $1 ]; then
     echo "Usage: build-web.sh {dev | start} {1 | 2}"
     exit 1
@@ -11,25 +9,24 @@ if [ -z $2 ]; then
 fi
 
 if [ $2 = "1" ]; then
+    cd ../..
     cargo build
 
-    cd ./web/wasm-chess
-    rustwasmc build --no-wasi --target deno --out-dir ../app/api/game/wasm ./
+    cd ./wasm
+    rustwasmc build --no-wasi --target deno --out-dir ../web/aleph-experimental/api/game/wasm ./
     cd ..
 
-    cd ./app
-    deno run --allow-read --allow-write build.ts
+    cd ./web/aleph-experimental
+    deno run --allow-read --allow-write prebuild.ts
 
     # reload files for an initial build
     rm -rf .aleph
     alephjs $1
 elif [ $2 = "2" ]; then
-    cd ./web/app
-
     ## server will eventually terminate via error if browser is open
     ## if not, issue SIGTERM and run next lines manually
     # adjust build to work
-    deno run --allow-read --allow-write postreload.ts development
+    deno run --allow-read --allow-write postbuild.ts development
 
     alephjs $1
 fi
