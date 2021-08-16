a25ec8ddafbe217ae8a7aa67be843bf5a2304d39
Sean Sullivan <sullivansean27@gmail.com>
2021-08-15 21:28:44 -0400

    rip importmap

--- web/server/deps.ts
+++ web/server/deps.ts
@@ -1,3 +1,3 @@
 // TODO: remove when deno deploy supports import maps
-export * from 'oak'
-export * from 'ws'
+export * from 'https://deno.land/x/oak@v8.0.0/mod.ts'
+export * from 'https://deno.land/std@0.102.0/ws/mod.ts'
