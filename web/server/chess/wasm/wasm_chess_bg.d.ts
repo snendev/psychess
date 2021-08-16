602d0eedc65ef6d0a0dc9262ab4eac1bfc8bcf80
Sean Sullivan <sullivansean27@gmail.com>
2021-08-15 21:26:04 -0400

    also add current wasm snapshot

---
+++ web/server/chess/wasm/wasm_chess_bg.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wasmclient_free(a: number): void;
+export function get_piece_from_i32(a: number, b: number): void;
+export function get_piece_index_from_character(a: number): number;
+export function create_board(a: number, b: number, c: number): number;
+export function wasmclient_new(): number;
+export function wasmclient_move_piece(a: number, b: number, c: number): number;
+export function wasmclient_get_valid_targets(a: number, b: number, c: number): void;
+export function wasmclient_is_white_turn(a: number): number;
+export function wasmclient_render_board(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
