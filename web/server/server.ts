98521f50cb6bf0e1f47dc785a70728e062e7b6a6
Sean Sullivan <sullivansean27@gmail.com>
2021-08-18 23:39:26 -0400

    more logs

--- web/server/server.ts
+++ web/server/server.ts
@@ -147,7 +147,7 @@ app.use(async (ctx, next) => {
   if (contentTypeValue) {
     headers.set('Content-Type', contentTypeValue)
   }
-  console.log({assetURL, contentTypeValue, pathname, headers})
+  console.log({assetURL, contentTypeValue, pathname, headers, res: response.body})
   return new Response(response.body, {...response, headers})
 })
 
