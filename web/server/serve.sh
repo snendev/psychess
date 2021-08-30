a9f66909406c686a7e4a48621f5867627c40e8a7
snen <seansullygames@gmail.com>
2021-08-30 01:53:07 -0400

    improve build scripts more

--- web/server/serve.sh
+++ web/server/serve.sh
@@ -1,2 +1,3 @@
 cd "$(dirname "$0")"
-deno run --allow-read --unstable --allow-env --allow-net --import-map import_map.json server.ts
+# deno run --allow-read --unstable --allow-env --allow-net --import-map import_map.json server.ts
+GITHUB_SECRET=$GITHUB_SECRET LOG_LEVEL=debug deployctl run server.ts --no-check
