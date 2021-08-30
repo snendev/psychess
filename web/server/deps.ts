55472a253b1668c5de04b2e1f1c1331a58d630ff
snen <seansullygames@gmail.com>
2021-08-29 23:40:25 -0400

    remove deps

--- web/server/deps.ts
+++ web/server/deps.ts
@@ -1,5 +1,4 @@
 // TODO: remove when deno deploy supports import maps
 export * from 'https://deno.land/x/oak@v8.0.0/mod.ts'
 export { contentType } from 'https://deno.land/x/media_types@v2.10.0/mod.ts'
-export {proxy as proxyMiddleware} from 'https://deno.land/x/oak_http_proxy@1.4.1/mod.ts'
 export * from 'https://deno.land/std@0.102.0/ws/mod.ts'
