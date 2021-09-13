11770d6d2a25cd9a8138a73d8e830caebfd767c8
snen <seansullygames@gmail.com>
2021-09-13 03:00:15 -0400

    test CSP concern

--- web/server/routes/static.tsx
+++ web/server/routes/static.tsx
@@ -31,6 +31,7 @@ const html = `
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="connect-src 'self' wss://host:port ws://host:port;" />
     <title>Psy(chic) Chess</title>
     <meta name="description" content="Wacky chess variant" />
     <style>
