f74cc7989c874ba09c6a289364588f1bf275328a
snen <seansullygames@gmail.com>
2021-09-13 19:55:24 -0400

    fix serve file

--- web/serve.sh
+++ web/serve.sh
@@ -1,3 +1,4 @@
 cd "$(dirname "$0")"
-# deno run --allow-read --unstable --allow-env --allow-net --import-map import_map.json ./server/server.ts
-GITHUB_SECRET=$GITHUB_SECRET LOG_LEVEL=debug deployctl run ./server/server.ts --no-check
+deno run --allow-read --unstable --allow-env --allow-net --import-map import_map.json ./server/server.ts
+# GITHUB_SECRET=$GITHUB_SECRET LOG_LEVEL=debug deployctl run ./server/server.ts --no-check
+# TODO: use deno/deployctl when Deno.emit is supported
