af5bec208064d6e5a06b4af829f8f06682ac982b
snen <seansullygames@gmail.com>
2021-09-21 23:19:39 -0400

    scripts: fix web server?

--- scripts/serve-web.sh
+++ scripts/serve-web.sh
@@ -1,5 +1,5 @@
 cd "$(dirname "$0")"
-cd ..
+cd ../web
 
 deno run --allow-read --unstable --allow-env --allow-net --import-map import_map.json ./server/server.ts
 # GITHUB_SECRET=$GITHUB_SECRET LOG_LEVEL=debug deployctl run ./server/server.ts --no-check
